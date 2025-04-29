09269357a566525998a50bf3edad4778e8268584
YaniraGS <yaniragonzalezsan@gmail.com>
2025-04-29 13:39:42 +0200

    conectando base de datos

--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -1,4 +1,6 @@
 import {Router} from 'express'
+import { pool } from  '../db.js';
+
 
 const router = Router();
 
@@ -16,7 +18,8 @@ router.post('/users', (req,res) =>{
     res.send("creando usuario")
 })
 router.put('/users/:id', (req,res) =>{
-    res.send("Obteniendo")
+    const {id} = req.params;
+    res.send("Actualizando usuarios")
 })
 router.delete('/users/:id', (req,res) =>{
     res.send("Obteniendo")
