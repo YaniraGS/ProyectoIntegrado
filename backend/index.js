c3be81897265d277496e281a9ac923ed45358382
YaniraGS <yaniragonzalezsan@gmail.com>
2025-05-06 09:35:23 +0200

    Recipes route/controller

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import {PORT} from "./config.js"
 import usersRoutes from './routes/users.routes.js';
-
+import recipeRoutes from './routes/recipes.routes.js';
 
 const app = express();
 app.use(express.json());
 app.use(usersRoutes);
+app.use(recipeRoutes)
 app.listen(PORT);
---
+++ backend/src/routes/recipes.routes.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { createRecipe, getRecipeById, getRecipes } from "../controllers/recipes.controller";
+
+const router = Router();
+
+router.get('/recipes',getRecipes);
+router.get ('/recipes/:id', getRecipeById);
+router.post('/recipes', createRecipe);
+
+export default router;
